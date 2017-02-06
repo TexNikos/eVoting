309dad5eb50bd74a8460ff7e38306b3901167475
TexNikos <texnikos.git@gmail.com>
2017-02-06 17:44:49 +0200

    DBManager Μικρές αλλαγές

--- Source/eVoting/src/db/DBManager.java
+++ Source/eVoting/src/db/DBManager.java
@@ -19,30 +19,29 @@ public class DBManager {
     private static EntityManager em;
     private static boolean active = false;
 
-    public static EntityManager create() {
-        
+    public static void create() {
+
         if (!active) {
-            return create("eVotingPU");
+            create("eVotingPU");
         }
 
-        return em;
-        
     }
 
-    public static EntityManager create(String pUnitName) {
-        
+    public static void create(String pUnitName) {
+
         if (!active) {
             emf = Persistence.createEntityManagerFactory(pUnitName);
             em = emf.createEntityManager();
             active = true;
         }
-
-        return em;
-        
     }
 
-    public static void Deactivate() {
-        
+    public EntityManager em() {
+        return em;
+    }
+
+    public static void destroy() {
+
         active = false;
         em.close();
         emf.close();
