4482c5276589932810a589ffcd92bd0a156fdc0f
TexNikos <texnikos.git@gmail.com>
2017-03-13 20:00:09 +0200

    Πίτα χωρίς φλουρί
    Ψήνεται....

--- Source/eVoting/src/gui/utilities/UtilFuncs.java
+++ Source/eVoting/src/gui/utilities/UtilFuncs.java
@@ -5,28 +5,40 @@
  */
 package gui.utilities;
 
+import db.DBManager;
+import dbentity.ElectoralPeriphery;
+import dbentity.PoliticalParty;
 import java.awt.Frame;
+import javax.persistence.TypedQuery;
 import javax.swing.JFrame;
 
 /**
  *
  * @author Nikos
  */
-public class UtilFuncs
-{
-    
-        public static Frame getDialogOwnerFrame()
-    {
+public class UtilFuncs {
+
+    public static Frame getDialogOwnerFrame() {
         Frame[] frames = JFrame.getFrames();
-        for (int i = 0; i < frames.length; i++)
-        {
+        for (int i = 0; i < frames.length; i++) {
             Frame frame = frames[i];
-            if (frame.isVisible())
-            {
+            if (frame.isVisible()) {
                 return frame;
             }
         }
         return null;
     }
     
+    public static ElectoralPeriphery getPeripheryByName(String peripheryName) {
+        TypedQuery<ElectoralPeriphery> q = DBManager.em().createNamedQuery("ElectoralPeriphery.findByFldName", ElectoralPeriphery.class);
+        q.setParameter("fldName", peripheryName);
+        return q.getSingleResult();
+    }
+
+    public static PoliticalParty getPoliticalPartyByName(String party) {
+        TypedQuery<PoliticalParty> q = DBManager.em().createNamedQuery("PoliticalParty.findByFldTitle", PoliticalParty.class);
+        q.setParameter("fldTitle", party);
+        return q.getSingleResult();
+    }
+
 }
